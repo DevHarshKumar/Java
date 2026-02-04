ca5349a03fd948126cc5b2d8b75287ea883e1622
DevHarshKumar <hkam02122001@gmail.com>
2026-02-04 15:18:58 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class First {
+    public static void main(String arg[]) {
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
