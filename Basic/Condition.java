76d46c566d3b9e5ffccb6a8c847f9184b136bbd2
DevHarshKumar <hkam02122001@gmail.com>
2026-02-07 14:50:14 +0530

    Question

--- Basic/Condition.java
+++ Basic/Condition.java
@@ -18,14 +18,46 @@ class Condition {
 
         // Even number
 
-        int n = sc.nextInt();
-        if (n % 2 == 0) {
-            System.out.println("The number is Even");
-        } else if (n == 0) {
-            System.out.println("The number is Zero");
-        } else {
-            System.out.println("Number is odd");
-        }
+        // int n = sc.nextInt();
+        // if (n % 2 == 0) {
+        // System.out.println("The number is Even");
+        // } else if (n == 0) {
+        // System.out.println("The number is Zero");
+        // } else {
+        // System.out.println("Number is odd");
+        // }
+
+        // System.out.print("Enter the weight of the water melon:");
+        // int w = sc.nextInt();
+        // String ans = w % 2 == 0 ? "Yes" : "No";
+
+        // System.out.println(ans);
+
+        // Program to check whether a number is negative, positive or zero.
+        // System.out.print("Enter your number:");
+        // int n=sc.nextInt();
+        // if(n<0){
+        // System.out.println("Number is Negative");
+        // }else if(n==0){
+        // System.out.println("Number is Zero");
+        // }else{
+        // System.out.println("Number is Positive");
+        // }
+
+        // Program to check whether a number is divisible by 5 and 11 or not.
+        // System.out.print("Enter Your Number:");
+        // int n = sc.nextInt();
+        // if (n % 5 == 0 && n % 11 == 0) {
+        // System.out.println("number is divisible by 5 and 11");
+        // } else {
+        // System.out.println("number is not divisible by 5 and 11");
+        // }
+
+        // Program to check whether a number is even or odd.
+        // System.out.print("Enter Your Number:");
+        // int n = sc.nextInt();
+        // String ans = n % 2 == 0 ? "Number is Even" : "Number is Odd";
+        // System.out.println(ans);
         sc.close();
     }
 }
