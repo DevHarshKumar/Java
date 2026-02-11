2cc9f9ec5255ba1e2b6ad8feeebc7e72b3615c3f
DevHarshKumar <hkam02122001@gmail.com>
2026-02-11 19:43:22 +0530

    completed condition & loop questions

--- Question.java
+++ Question.java
@@ -20,13 +20,55 @@ public class Question {
         // }
 
         // Check whether a character is alphabet or digit
-        char c = sc.next().charAt(0);
-        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
-            System.out.println("It is an Alphabet");
-        } else if (c >= '0' && c <= '9') {
-            System.out.println("It is a Digit");
-        } else {
-            System.out.println("It is a Special character");
+        // char c = sc.next().charAt(0);
+        // if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        // System.out.println("It is an Alphabet");
+        // } else if (c >= '0' && c <= '9') {
+        // System.out.println("It is a Digit");
+        // } else {
+        // System.out.println("It is a Special character");
+        // }
+
+        // Enter week number and print day of the week
+        // int n = sc.nextInt();
+
+        // switch (n) {
+        // case 0:
+        // System.out.println("Sunday");
+        // break;
+        // case 1:
+        // System.out.println("Monday");
+        // break;
+        // case 2:
+        // System.out.println("Tuesday");
+        // break;
+        // case 3:
+        // System.out.println("Wednesday");
+        // break;
+        // case 4:
+        // System.out.println("Thursday");
+        // break;
+        // case 5:
+        // System.out.println("Friday");
+        // break;
+        // case 6:
+        // System.out.println("Saturday");
+        // break;
+        // default:
+        // System.out.println("Wrong Input");
+        // break;
+        // }
+
+        // Print all Odd numbers from 1 to n
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i = i + 2) {
+        // System.out.print(i + " ");
+        // }
+
+        // Print multiplication table of a given number
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + (n * i));
         }
         sc.close();
     }
