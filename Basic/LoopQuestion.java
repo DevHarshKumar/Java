14a2568cd343623d05acec2f1e41a10a3b4fbfc7
DevHarshKumar <hkam02122001@gmail.com>
2026-02-14 22:49:54 +0530

    q

---
+++ Basic/LoopQuestion.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class LoopQuestion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Count the number of digits of a positive number.
+
+        // int num = sc.nextInt();
+        // if (num == 0) {
+        // System.out.println("1");
+        // return;
+        // }
+        // int count = 0;
+        // while (num > 0) {
+        // count++;
+
+        // int n;
+        // n = num % 10;
+        // num = num / 10;
+        // }
+        // System.out.println("No. of digits:" + count);
+
+        // A sum of digits of a positive number.
+        // int number = sc.nextInt();
+        // int sum = 0;
+        // while (number > 0) {
+        // int n = number % 10;
+        // sum = sum + n;
+        // number = number / 10;
+        // }
+        // System.out.println("The Sum is :" + sum);
+
+        // Sum of digits of all numbers from 1 to N.
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // for (int i = 1; i <= n; i++) {
+        // int sum1 = 0;
+        // int num1 = i;
+        // while (num1 > 0) {
+        // int num = num1 % 10;
+        // sum1 = sum1 + num;
+        // num1 = num1 / 10;
+        // }
+        // sum = sum + sum1;
+        // }
+        // System.out.println("The Sum is :" + sum);
+
+        // Check whether a number is palindrome or not
+        // int number = sc.nextInt();
+        // int number1 = number;
+        // int number2 = 0;
+        // while (number1 > 0) {
+        //     int n = number1 % 10;
+        //     number2 = (number2 * 10) + n;
+        //     number1 = number1 / 10;
+        // }
+        // if (number2 == number) {
+        //     System.out.println("Number is palindrome");
+        // } else {
+        //     System.out.println("Number is not Palindrome");
+        // }
+
+        //Check if a given number is prime or not?
+        int n=sc.nextInt();
+        int count=0;
+        for()
+        sc.close();
+    }
+}
