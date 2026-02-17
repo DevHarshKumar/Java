277929d070e458975b7ef135cd6b14e4f25eb1e8
DevHarshKumar <hkam02122001@gmail.com>
2026-02-17 11:09:41 +0530

    question

---
+++ Basic/AllPrime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class AllPrime {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 1; i <= number; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
+}
---
+++ Basic/ProductDigit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ProductDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 0 || n == 1) {
+            System.out.println(n);
+            return;
+        }
+        int pro = 1;
+        while (n >= 1) {
+            int num = n % 10;
+            pro = pro * num;
+            n = n / 10;
+        }
+        System.out.println("Product:" + pro);
+        sc.close();
+    }
+}
